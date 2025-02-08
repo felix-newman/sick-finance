c4f8770e2ffd83c22961adc700297b7959cbe157
Caspar <113780267+Caspar-Progius@users.noreply.github.com>
2025-02-08 16:48:48 +0100

    adjust from weather to crawl

--- restack-app/src/workflows/multistep.py
+++ restack-app/src/workflows/multistep.py
@@ -4,31 +4,31 @@ from datetime import timedelta
 
 with import_functions():
     from src.functions.llm import llm, FunctionInputParams
-    from src.functions.weather import weather
+    from src.functions.crawl import crawl
 
 class WorkflowInputParams ( BaseModel):
-    name: str = Field(default="John Doe")
+    news_url: str = Field(default="https://www.globenewswire.com/newsroom")
 
 @workflow.defn()
 class MultistepWorkflow:
     @workflow.run
     async def run(self, input: WorkflowInputParams):
         log.info("MultistepWorkflow started", input=input)
-        user_content = f"Greet this person {input.name}"
-
-        # Step 1 get weather data
-        weather_data = await workflow.step(
-            weather,
+        # Step 1 get article data
+        article_content = await workflow.step(
+            crawl,
+            FunctionInputParams(
+                url=input.news_url
+            ),
             start_to_close_timeout=timedelta(seconds=120)
         )
 
-        # Step 2 Generate greeting with LLM  based on name and weather data
-
+        # Step 2 Generate summary with LLM 
         llm_message = await workflow.step(
             llm,
             FunctionInputParams(
-                system_content=f"You are a personal assitant and have access to weather data {weather_data}. Always greet person with relevant info from weather data",
-                user_content=user_content,
+                system_content=f"You are a news information assistant. Summarize the most relevant information from any news article you get",
+                user_content=article_content,
                 model="gpt-4o-mini"
             ),
             start_to_close_timeout=timedelta(seconds=120)
@@ -36,5 +36,5 @@ class MultistepWorkflow:
         log.info("MultistepWorkflow completed", llm_message=llm_message)
         return {
             "message": llm_message,
-            "weather": weather_data
+            "article_content": article_content
         }
