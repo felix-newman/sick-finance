30931133ad6c24811447e33fa9c312b52f63495b
Bnowako <blazejnowakowski98@gmail.com>
2025-02-09 11:28:25 +0100

    more styles

--- front/src/app/[title]/page.tsx
+++ front/src/app/[title]/page.tsx
@@ -4,25 +4,33 @@ import Link from "next/link";
 import Image from 'next/image'
 
 export default async function Home({ params }: { params: { title: string } }) {
-    
+
     const article = await getGeneratedArticleByTitle(params.title);
-    
+
     return (
         <div>
             <div className="container mx-auto p-4">
                 <div className="max-w-[50%] mx-auto">
-                    <h1>{article.title}</h1>
-                    <p>{article.lead}</p>
-                    <Image
-                    width={350}
-                    height={350}
-                    src={`data:image/jpeg;base64,${article.image_data}`} 
-                    alt={article.title} 
-                    className="my-4" />
-                    <div>{article.content}</div>
+                    <div className="">
+                        <img
+                            width={200}
+                            height={200}
+                            src={`data:image/jpeg;base64,${article.image_data}`}
+                            alt={article.title}
+                            className="my-4 mx-auto" />
+                    </div>
+
+                    <h1 className="text-4xl font-bold">{article.title}</h1>
+
+
+                    <blockquote className="my-4 ml-[0.075em] border-l-3 border-gray-300 pl-4 text-gray-700">{article.lead}</blockquote>
+
+
+                    <p className="text-gray-800 leading-snug">{article.content}</p>
+
                     <ul>
                         {article.mentioned_stocks?.map(stock => (
-                          <li key={stock}>{stock}</li>
+                            <li key={stock}>{stock}</li>
                         ))}
                     </ul>
                 </div>
